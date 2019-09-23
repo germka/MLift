188cb85bbf99470bcceaf621926ce795deb91637
germka <germka@gmail.com>
2019-09-23 13:18:02 +0300

    Ticket index page renewed with paginator & comment view is optimised

--- base/urls.py
+++ base/urls.py
@@ -4,12 +4,9 @@ from . import views
 app_name = 'base'
 urlpatterns = [
     # Список заявок
-    path('', views.TicketIndexView.as_view(), name='index'),
+    path('', views.ticket_index, name='ticket_index'),
     # Окно заявки
-#    path('<int:pk>/', views.TicketDetailView.as_view(), name='ticket_detail'), 
     path('<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
-    # Добавление комментария
-#    path('<int:ticket_id>/', views.new_comment, name='new_comment'), comment add form moved to detail view
     # Создание заявки
     path('newticket/', views.new_ticket, name='new_ticket'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.utils import timezone
-from django.views import generic
+#from django.views import generic
+from django.core.paginator import Paginator
 #from django.template import loader
 
 from .models import Ticket, TicketStatus, Object, ObjStr, ObjType, ManageComp
 
 # Create your views here.
 
-class TicketIndexView(generic.ListView):
-    template_name = 'base/index.html'
-    context_object_name = 'latest_ticket_list'
+def ticket_index(request):
+    paginator = Paginator(Ticket.objects.order_by('-ticket_date'), 20)
 
-    def get_queryset(self):
-        return Ticket.objects.order_by('-ticket_date')[:20]
+    page = request.GET.get('page')
+    ticket_list = paginator.get_page(page)
+    context = {
+        'ticket_list': ticket_list,
+    }
+    return render(request, 'base/index.html', context)
 
 
 def ticket_detail(request, ticket_id):
@@ -22,19 +26,21 @@ def ticket_detail(request, ticket_id):
     context = {
         'ticket': ticket,
     }
-    try:
+    if request.method == 'GET':
+        return render(request, 'base/detail.html', context)
+    if request.method == 'POST':
         content = request.POST['comment_content']
-    except (KeyError, Ticket.DoesNotExist):
-        return render(request, 'base/detail.html', { 'ticket': ticket, })
-    else:
+        try:
+            last_content = ticket.comments_set.last().comment_content
+        except:
+            last_content = null
         if content != "":
-            if content != ticket.comments_set.last().comment_content:
+            if content != last_content:
                 ticket.comments_set.create(comment_content=content, comment_date=timezone.now())
-                return render(request, 'base/detail.html', { 'ticket': ticket, 'message':"Комментарий успешно добавлен", })
-            else:
-                return render(request, 'base/detail.html', { 'ticket': ticket, })
+                context['message'] = "Комментарий успешно добавлен"
         else:
-            return render(request, 'base/detail.html', { 'ticket': ticket, 'errormessage':"Комментарий не должен быть пустым", })
+            context['errormessage'] = "Комментарий не должен быть пустым"
+        return render(request, 'base/detail.html', context)
 
 
 def new_ticket(request):
@@ -65,14 +71,3 @@ def new_ticket(request):
             ticket.save()
             return HttpResponseRedirect(reverse('base:index'))
 
-
-#def new_comment(request, ticket_id):
-#    try:
-#        content = request.POST['comment_content']
-#        ticket = Ticket.objects.get(pk=ticket_id)
-#    except (KeyError, Ticket.DoesNotExist):
-#        return render(request, 'base/detail.html', { 'errormessage':"Комментарий не должен быть пустым", })
-#    else:
-#        if content != "":
-#            ticket.comment_set.create(comment_content=content, comment_date=timezone.now())
-#            return render(request, 'base/detail.html', { 'message':"Комментарий успешно добавлен", })
