a14ba7c9c4183265ecfdaa04d60d12e1f394d7ee
germka <germka@gmail.com>
2019-09-28 17:28:07 +0300

    redisined detail view and fixed newticket, but need pay attention to obj_type exact choise filter

--- base/views.py
+++ base/views.py
@@ -25,11 +25,31 @@ def ticket_index(request):
 
 def ticket_detail(request, ticket_id):
     ticket = get_object_or_404(Ticket, pk=ticket_id)
-    ticket_object = Object.objects.get(pk=ticket.ticket_object.id)
     context = {
         'ticket': ticket,
-        'ticket_object': ticket_object,
+        'obj_type': ObjType.objects.all(),
     }
+
+    try:
+        ticket_object = Object.objects.get(pk=ticket.ticket_object.id)
+    except:
+        context['no_object_message'] = "Лифт пока не определен"
+    else:
+        context['ticket_object'] = ticket_object
+
+    try:
+        ticket_object_exact = Object.objects.filter(obj_str=ticket.ticket_str, obj_build=ticket.ticket_build, obj_biuld_housing=ticket.ticket_build_housing, obj_par=ticket.ticket_par)
+    except:
+        context['error_message'] = "Объект с таким адресом не найден"
+    else:
+        try:
+            ticket.ticket_object = ticket_object_exact.get()
+        except (KeyError, Object.DoesNotExist):
+            content['exact_message'] = "Выберите тип лифта"
+            context['object_exact'] = ticket_object_exact.obj_type.values('obj_type').order_by('obj_type').distinct()
+        else:
+            context['exact_message'] = "Лифт определен"
+
     if request.method == 'GET':
         return render(request, 'base/detail.html', context)
     if request.method == 'POST':
@@ -82,7 +102,7 @@ def new_ticket(request):
 #--obj filters
 
         try:
-            new_objstr = ObjStr.objects.get(street=request.POST['obj_str']).id
+            new_objstr = ObjStr.objects.get(street=request.POST['obj_str'])
         except(KeyError, ObjStr.DoesNotExist):
             context['error_message'] = "Значение улицы не заполнено"
 
@@ -92,7 +112,10 @@ def new_ticket(request):
             context['error_message'] = "Значение номера дома не заполнено"
 
 
-        new_objbuildhousing = request.POST['obj_build_housing']
+        try:
+            new_objbuildhousing = request.POST['obj_build_housing']
+        except:
+            new_objbuildhousing = ''
 
 
         new_objpar = request.POST['obj_par']
@@ -101,7 +124,7 @@ def new_ticket(request):
 
 
         try:
-            new_objtype = ObjType.objects.get(type_name=request.POST['obj_type']).id
+            new_objtype = ObjType.objects.get(type_name=request.POST['obj_type'])
         except(KeyError, ObjType.DoesNotExist):
             context['error_message'] = "Значение типа лифта не верно"
 
